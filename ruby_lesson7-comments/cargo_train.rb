c5ef219c7d13d8cb1759bd3673e78dd050c09d09
Evgeniy Fateev <spaceflow@gmail.com>
2017-06-14 13:53:12 +0300

    Add lesson-7 comments

---
+++ ruby_lesson7-comments/cargo_train.rb
@@ -0,0 +1,9 @@
+require_relative 'train'
+
+class CargoTrain < Train
+
+  def initialize(number)
+    super(number, :cargo)
+  end
+
+end
---
+++ ruby_lesson7-comments/vagon.rb
@@ -0,0 +1,12 @@
+require_relative 'module_vendor_name'
+
+class Vagon
+
+  include VendorName
+
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+end
