163fdea34d90b7847d76f99cde22f8d32926689c
Andrey Makarenko <photobitmiitsupp@gmail.com>
2017-06-20 01:23:43 +0300

    global var

--- ruby_lesson8/module_accessors.rb
+++ ruby_lesson8/module_accessors.rb
@@ -3,13 +3,13 @@ module Acessors
     $history = {}
     names.each do |name|
       var_name = "@#{name}".to_sym
-      $history[var_name]=[]
+      $history[name.to_sym]=[]
       define_method(name) { instance_variable_get(var_name) }
       define_method("#{name}=".to_sym) do |value|
         instance_variable_set(var_name, value)
-        $history[var_name]=value
+        $history[name.to_sym]<<value
       end
-      define_method("#{name}_history".to_sym) { puts $history["#{name}".to_sym] }
+      define_method("#{name}_history".to_sym) { puts $history[name.to_sym] }
     end
   end
 end
@@ -21,4 +21,6 @@ end
 
 m = Test.new
 m.speak=2
-puts m.speak
+m.speak=4
+m.speak=1290441
+m.speak_history
